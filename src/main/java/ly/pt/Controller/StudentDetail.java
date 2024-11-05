3bb6edc5081c584ae61b04bb38ee9b87edb00253
gihanviduranga <gihanvidu123@gmai.com>
2024-11-05 14:35:16 +0530

    Student Detail is done

---
+++ src/main/java/ly/pt/Controller/StudentDetail.java
@@ -0,0 +1,71 @@
+package ly.pt.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class StudentDetail {
+
+    @FXML
+    private AnchorPane Root;
+
+    @FXML
+    private DatePicker dtpDateOfBirth;
+
+    @FXML
+    private Label lblStudentID;
+
+    @FXML
+    private TableColumn<?, ?> tblAddress;
+
+    @FXML
+    private TableColumn<?, ?> tblDateOfBirth;
+
+    @FXML
+    private TableColumn<?, ?> tblEmail;
+
+    @FXML
+    private TableColumn<?, ?> tblName;
+
+    @FXML
+    private TableColumn<?, ?> tblPhoneNumber;
+
+    @FXML
+    private TableView<?> tblProgramTable;
+
+    @FXML
+    private TableColumn<?, ?> tblStudentID;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtPhoneNumber;
+
+    @FXML
+    private TextField txtStudentName;
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+}
